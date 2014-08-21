bc80b46bb05a36c19f24c65f3a3837e22e6ea767
wescleyws <wescley.gomes@dce.ufpb.br>
2014-08-21 18:26:01 -0300

    Update Cadastros.java

--- src/Cadastros.java
+++ src/Cadastros.java
@@ -4,12 +4,7 @@ import java.util.ArrayList;
 public class Cadastros {
 	
 	
-public void impirmir(){
-	
-}
-	 
-	
-	
+
 public String Cadastrar(Cadastrador cadastrador){
 		return cadastrador.Cadastrar();
 		
